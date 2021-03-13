f275b72d8ac50ac1e908c5b00621b12e77c1feed
GPinchukov <pinchukov000@gmail.com>
2021-03-13 21:33:32 +0300

    ssss

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 module.exports = function reverse (n) {
-    let new_n = n.toString().split('').reverse().join('');
-    return parseInt(new_n);
+    let ar = n.toString().split('').reverse().join('');
+    return parseInt(ar);
 }
